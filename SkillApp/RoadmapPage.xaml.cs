18c6a224893698658fbe2301027bfc9f81e7ee69
mykhailolyt <mykhailo.lytvynenko.kb.2022@lpnu.ua>
2023-12-27 10:11:40 +0200

    Redesigned main page, commented out broken code.

--- SkillApp/RoadmapPage.xaml.cs
+++ SkillApp/RoadmapPage.xaml.cs
@@ -10,187 +10,187 @@ public partial class RoadmapPage : ContentPage
     private void OnCreateRoadmapClicked(object sender, EventArgs e)
     {
 
-        foreach (var task in obj.roadmap.beginner.tasks)
-        {
-            var labelBegginerTask = new Label
-            {
-                Text = task.task,
-                FontSize = 16,
-                Margin = new Thickness(10, 10, 0, 0),
-                TextColor = Colors.White,
-            };
-            labelStackLayout.Children.Add(labelBegginerTask);
+        //foreach (var task in obj.roadmap.beginner.tasks)
+        //{
+        //    var labelBegginerTask = new Label
+        //    {
+        //        Text = task.task,
+        //        FontSize = 16,
+        //        Margin = new Thickness(10, 10, 0, 0),
+        //        TextColor = Colors.White,
+        //    };
+        //    labelStackLayout.Children.Add(labelBegginerTask);
 
-            foreach (var subtask in task.subtasks)
-            {
-                var labelBegginerSubtask = new Label
-                {
-                    Text = subtask,
-                    FontSize = 16,
-                    Margin = new Thickness(10, 10, 0, 0),
-                    TextColor = Colors.White,
-                };
+        //    foreach (var subtask in task.subtasks)
+        //    {
+        //        var labelBegginerSubtask = new Label
+        //        {
+        //            Text = subtask,
+        //            FontSize = 16,
+        //            Margin = new Thickness(10, 10, 0, 0),
+        //            TextColor = Colors.White,
+        //        };
 
-                labelStackLayout.Children.Add(labelBegginerSubtask);
-            }
+        //        labelStackLayout.Children.Add(labelBegginerSubtask);
+        //    }
 
-            foreach (var resource in task.resources)
-            {
-                var labelBegginerResource = new Label
-                {
-                    Text = resource.resource,
-                    FontSize = 16,
-                    Margin = new Thickness(10, 10, 0, 0),
-                    TextColor = Colors.White,
-                };
+        //    foreach (var resource in task.resources)
+        //    {
+        //        var labelBegginerResource = new Label
+        //        {
+        //            Text = resource.resource,
+        //            FontSize = 16,
+        //            Margin = new Thickness(10, 10, 0, 0),
+        //            TextColor = Colors.White,
+        //        };
 
-                labelStackLayout.Children.Add(labelBegginerResource);
+        //        labelStackLayout.Children.Add(labelBegginerResource);
 
-                var labelBegginerUrl = new Label
-                {
-                    Text = resource.url,
-                    FontSize = 16,
-                    Margin = new Thickness(10, 10, 0, 0),
-                    TextColor = Colors.White,
-                };
+        //        var labelBegginerUrl = new Label
+        //        {
+        //            Text = resource.url,
+        //            FontSize = 16,
+        //            Margin = new Thickness(10, 10, 0, 0),
+        //            TextColor = Colors.White,
+        //        };
 
-                labelStackLayout.Children.Add(labelBegginerUrl);
-            }
+        //        labelStackLayout.Children.Add(labelBegginerUrl);
+        //    }
 
-            foreach (var milestone in obj.roadmap.beginner.milestones)
-            {
-                var labelBegginerMilestones = new Label
-                {
-                    Text = milestone,
-                    FontSize = 16,
-                    Margin = new Thickness(10, 10, 0, 0),
-                    TextColor = Colors.White,
-                };
+        //    foreach (var milestone in obj.roadmap.beginner.milestones)
+        //    {
+        //        var labelBegginerMilestones = new Label
+        //        {
+        //            Text = milestone,
+        //            FontSize = 16,
+        //            Margin = new Thickness(10, 10, 0, 0),
+        //            TextColor = Colors.White,
+        //        };
 
-                labelStackLayout.Children.Add(labelBegginerMilestones);
-            }
-        }
+        //        labelStackLayout.Children.Add(labelBegginerMilestones);
+        //    }
+        //}
 
-        foreach (var task in obj.roadmap.intermediate.tasks)
-        {
-            var labelIntermediateTask = new Label
-            {
-                Text = task.task,
-                FontSize = 16,
-                Margin = new Thickness(10, 10, 0, 0),
-                TextColor = Colors.White,
-            };
-            labelStackLayout.Children.Add(labelIntermediateTask);
+        //foreach (var task in obj.roadmap.intermediate.tasks)
+        //{
+        //    var labelIntermediateTask = new Label
+        //    {
+        //        Text = task.task,
+        //        FontSize = 16,
+        //        Margin = new Thickness(10, 10, 0, 0),
+        //        TextColor = Colors.White,
+        //    };
+        //    labelStackLayout.Children.Add(labelIntermediateTask);
 
-            foreach (var subtask in task.subtasks)
-            {
-                var labelIntermediateSubtask = new Label
-                {
-                    Text = subtask,
-                    FontSize = 16,
-                    Margin = new Thickness(10, 10, 0, 0),
-                    TextColor = Colors.White,
-                };
+        //    foreach (var subtask in task.subtasks)
+        //    {
+        //        var labelIntermediateSubtask = new Label
+        //        {
+        //            Text = subtask,
+        //            FontSize = 16,
+        //            Margin = new Thickness(10, 10, 0, 0),
+        //            TextColor = Colors.White,
+        //        };
 
-                labelStackLayout.Children.Add(labelIntermediateSubtask);
-            }
+        //        labelStackLayout.Children.Add(labelIntermediateSubtask);
+        //    }
 
-            foreach (var resource in task.resources)
-            {
-                var labelIntermediateResource = new Label
-                {
-                    Text = resource.resource,
-                    FontSize = 16,
-                    Margin = new Thickness(10, 10, 0, 0),
-                    TextColor = Colors.White,
-                };
+        //    foreach (var resource in task.resources)
+        //    {
+        //        var labelIntermediateResource = new Label
+        //        {
+        //            Text = resource.resource,
+        //            FontSize = 16,
+        //            Margin = new Thickness(10, 10, 0, 0),
+        //            TextColor = Colors.White,
+        //        };
 
-                labelStackLayout.Children.Add(labelIntermediateResource);
+        //        labelStackLayout.Children.Add(labelIntermediateResource);
 
-                var labelIntermediateUrl = new Label
-                {
-                    Text = resource.url,
-                    FontSize = 16,
-                    Margin = new Thickness(10, 10, 0, 0),
-                    TextColor = Colors.White,
-                };
+        //        var labelIntermediateUrl = new Label
+        //        {
+        //            Text = resource.url,
+        //            FontSize = 16,
+        //            Margin = new Thickness(10, 10, 0, 0),
+        //            TextColor = Colors.White,
+        //        };
 
-                labelStackLayout.Children.Add(labelIntermediateUrl);
-            }
+        //        labelStackLayout.Children.Add(labelIntermediateUrl);
+        //    }
 
-            foreach (var milestone in obj.roadmap.intermediate.milestones)
-            {
-                var labelIntermediateMilestones = new Label
-                {
-                    Text = milestone,
-                    FontSize = 16,
-                    Margin = new Thickness(10, 10, 0, 0),
-                    TextColor = Colors.White,
-                };
+        //    foreach (var milestone in obj.roadmap.intermediate.milestones)
+        //    {
+        //        var labelIntermediateMilestones = new Label
+        //        {
+        //            Text = milestone,
+        //            FontSize = 16,
+        //            Margin = new Thickness(10, 10, 0, 0),
+        //            TextColor = Colors.White,
+        //        };
 
-                labelStackLayout.Children.Add(labelIntermediateMilestones);
-            }
-        }
+        //        labelStackLayout.Children.Add(labelIntermediateMilestones);
+        //    }
+        //}
 
-        foreach (var task in obj.roadmap.advanced.tasks)
-        {
-            var labelAdvancedTask = new Label
-            {
-                Text = task.task,
-                FontSize = 16,
-                Margin = new Thickness(10, 10, 0, 0),
-                TextColor = Colors.White,
-            };
-            labelStackLayout.Children.Add(labelAdvancedTask);
+        //foreach (var task in obj.roadmap.advanced.tasks)
+        //{
+        //    var labelAdvancedTask = new Label
+        //    {
+        //        Text = task.task,
+        //        FontSize = 16,
+        //        Margin = new Thickness(10, 10, 0, 0),
+        //        TextColor = Colors.White,
+        //    };
+        //    labelStackLayout.Children.Add(labelAdvancedTask);
 
-            foreach (var subtask in task.subtasks)
-            {
-                var labelAdvancedSubtask = new Label
-                {
-                    Text = subtask,
-                    FontSize = 16,
-                    Margin = new Thickness(10, 10, 0, 0),
-                    TextColor = Colors.White,
-                };
+        //    foreach (var subtask in task.subtasks)
+        //    {
+        //        var labelAdvancedSubtask = new Label
+        //        {
+        //            Text = subtask,
+        //            FontSize = 16,
+        //            Margin = new Thickness(10, 10, 0, 0),
+        //            TextColor = Colors.White,
+        //        };
 
-                labelStackLayout.Children.Add(labelAdvancedSubtask);
-            }
+        //        labelStackLayout.Children.Add(labelAdvancedSubtask);
+        //    }
 
-            foreach (var resource in task.resources)
-            {
-                var labelAdvancedResource = new Label
-                {
-                    Text = resource.resource,
-                    FontSize = 16,
-                    Margin = new Thickness(10, 10, 0, 0),
-                    TextColor = Colors.White,
-                };
+        //    foreach (var resource in task.resources)
+        //    {
+        //        var labelAdvancedResource = new Label
+        //        {
+        //            Text = resource.resource,
+        //            FontSize = 16,
+        //            Margin = new Thickness(10, 10, 0, 0),
+        //            TextColor = Colors.White,
+        //        };
 
-                labelStackLayout.Children.Add(labelAdvancedResource);
+        //        labelStackLayout.Children.Add(labelAdvancedResource);
 
-                var labelAdvancedUrl = new Label
-                {
-                    Text = resource.url,
-                    FontSize = 16,
-                    Margin = new Thickness(10, 10, 0, 0),
-                    TextColor = Colors.White,
-                };
+        //        var labelAdvancedUrl = new Label
+        //        {
+        //            Text = resource.url,
+        //            FontSize = 16,
+        //            Margin = new Thickness(10, 10, 0, 0),
+        //            TextColor = Colors.White,
+        //        };
 
-                labelStackLayout.Children.Add(labelAdvancedUrl);
-            }
+        //        labelStackLayout.Children.Add(labelAdvancedUrl);
+        //    }
 
-            foreach (var milestone in obj.roadmap.advanced.milestones)
-            {
-                var labelAdvancedMilestones = new Label
-                {
-                    Text = milestone,
-                    FontSize = 16,
-                    Margin = new Thickness(10, 10, 0, 0),
-                    TextColor = Colors.White,
-                };
+        //    foreach (var milestone in obj.roadmap.advanced.milestones)
+        //    {
+        //        var labelAdvancedMilestones = new Label
+        //        {
+        //            Text = milestone,
+        //            FontSize = 16,
+        //            Margin = new Thickness(10, 10, 0, 0),
+        //            TextColor = Colors.White,
+        //        };
 
-                labelStackLayout.Children.Add(labelAdvancedMilestones);
-            }
-        }
+        //        labelStackLayout.Children.Add(labelAdvancedMilestones);
+        //    }
+        //}
     }
 }
