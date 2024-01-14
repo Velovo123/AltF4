2e86988517fc955553f4cc6d034b3c01e72a8d10
mykhailolyt <mykhailo.lytvynenko.kb.2022@lpnu.ua>
2024-01-14 14:36:30 +0200

    Added a loading "animation"

--- SkillApp/RequestPage.xaml.cs
+++ SkillApp/RequestPage.xaml.cs
@@ -10,19 +10,20 @@ public partial class RequestPage : ContentPage
         this.requestField = requestField;
         this.desiredKnowledgeLevel = desiredKnowledgeLevel;
 	}
-    //async void NavigateRoadmapPage(System.Object sender, System.EventArgs e)
+    async void NavigateRoadmapPage(System.Object sender, System.EventArgs e)
+    {
+        string requestGeneral = RequestEntry.Text;
+        CompleteBtn.Text = "Loading...";
+        var obj = await Operator.GenerateRoadMap(requestField, desiredKnowledgeLevel, requestGeneral);
+        await Navigation.PushAsync(new RoadmapPage(obj), true);
+    }
+
+    //async void NavigatePleaseWaitPage(System.Object sender, System.EventArgs e)
     //{
     //    string requestGeneral = RequestEntry.Text;
     //    var obj = await Operator.GenerateRoadMap(requestField, desiredKnowledgeLevel, requestGeneral);
-    //    await Navigation.PushAsync(new RoadmapPage(obj), true);
+    //    await Navigation.PushAsync(new PleaseWaitPage(obj), true);
     //}
-    async void NavigatePleaseWaitPage(System.Object sender, System.EventArgs e)
-    {
-        string requestGeneral = RequestEntry.Text;
-        var obj = await Operator.GenerateRoadMap(requestField, desiredKnowledgeLevel, requestGeneral);
-        
-        await Navigation.PushAsync(new PleaseWaitPage(obj), true);
-    }
     async void NatigateBack(System.Object sender, System.EventArgs e)
     {
         await Navigation.PopAsync();
---
+++ SkillApp/RoadmapLevel1.xaml.cs
@@ -0,0 +1,9 @@
+namespace SkillApp;
+
+public partial class RoadmapLevel1 : ContentPage
+{
+	public RoadmapLevel1()
+	{
+		InitializeComponent();
+	}
+}
